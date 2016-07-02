571820f4fa1035aeeb5a117a8ca27ba81356edbe
Alejandro Ruiz Varela <elgoberlivel@gmail.com>
2016-07-01 23:07:29 -0500

    Beacon class added

---
+++ BeaconScanner/Beacon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace BeaconScanner
+{
+	public class Beacon : IBeacon
+	{
+		internal Beacon(double rssi, int major, int minor, string name, Proximity proximity, string uuid)
+		{
+		}
+
+		#region IBeacon
+
+		public double Distance
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Major
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Minor
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public Proximity Proximity
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string UUID
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
+	}
+}
+
