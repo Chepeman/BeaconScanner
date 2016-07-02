141f24fc69c9c646dec246c4647f8bb8de743aae
Alejandro Ruiz Varela <elgoberlivel@gmail.com>
2016-07-01 22:19:24 -0500

    Abstractions Added

---
+++ BeaconScanner/IBeacon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BeaconScanner
+{
+	public enum Proximity
+	{
+		Near,
+		Immediate,
+		Far,
+		Unknown
+	}
+
+	public interface IBeacon
+	{
+		string UUID { get; }
+
+		double Distance { get; }
+
+		Proximity Proximity { get; }
+
+		int Major { get; }
+
+		int Minor { get; }
+
+		string Name { get; }
+	}
+}
+
---
+++ BeaconScanner/IBeaconManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeaconScanner
+{
+	public interface IBeaconManager
+	{
+		bool Init();
+
+		void AddRegion(string id, string uuid, int major = -1, int minor = -1);
+
+		void StartScan();
+
+		void StopScan();
+
+		void SetRangedAction(Action<IEnumerable<IBeacon>> action);
+	}
+}
+
