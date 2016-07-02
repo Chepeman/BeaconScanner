e209c07b4202c491f76dcd4cc0590f37a4bb49b8
JCarlosCS <carlos@orionsdev.com>
2016-07-01 22:02:43 -0500

    First commit

---
+++ BeaconScanner/BeaconScanner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace BeaconScanner
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+
+			MainPage = new NavigationPage(new ScanPage());
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ BeaconScanner/ScanPage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace BeaconScanner
+{
+	public class ScanPage : ContentPage
+	{
+		Button _scanButton;
+		ListView _beaconsList;
+		public ScanPage()
+		{
+			this.Title = "Beacon Scanner";
+
+			var beaconDataTemplate = new DataTemplate(() =>
+			{
+				var identifierLabel = new Label { HorizontalTextAlignment = TextAlignment.Start };
+				var proximityLabel = new Label { HorizontalTextAlignment = TextAlignment.End };
+
+				identifierLabel.SetBinding(Label.TextProperty, "Uuid");
+				proximityLabel.SetBinding(Label.TextProperty, "Proximity");
+
+				var beaconInfo = new StackLayout();
+				beaconInfo.Orientation = StackOrientation.Horizontal;
+				beaconInfo.Children.Add(identifierLabel);
+				beaconInfo.Children.Add(proximityLabel);
+
+				return new ViewCell { View = beaconInfo };
+			});
+
+			_scanButton = new Button();
+			_scanButton.IsEnabled = false;
+			_scanButton.Text = "Start Scan";
+			_scanButton.Clicked += _scanButton_Clicked;
+			_scanButton.HeightRequest = 80;
+
+			_beaconsList = new ListView();
+			_beaconsList.VerticalOptions = LayoutOptions.FillAndExpand;
+			_beaconsList.ItemTemplate = beaconDataTemplate;
+
+
+			var container = new StackLayout();
+			container.HorizontalOptions = LayoutOptions.FillAndExpand;
+			container.VerticalOptions = LayoutOptions.FillAndExpand;
+			container.Children.Add(_scanButton);
+			container.Children.Add(_beaconsList);
+
+
+			this.Content = container;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+		}
+
+		/*private async Task InitializeManager()
+		{
+			var status = await EstimoteManager.Instance.Initialize(); // optionally pass false to authorize foreground ranging only
+			if (status != BeaconInitStatus.Success)
+			{
+				await this.DisplayAlert("Error!", "Authorize the permissions!", "Ok");
+				_scanButton.IsEnabled = false;
+				return;
+			}
+			_scanButton.IsEnabled = true;
+			EstimoteManager.Instance.Ranged += Instance_Ranged;
+			EstimoteManager.Instance.RegionStatusChanged += Instance_RegionStatusChanged;
+
+		}
+
+		void Instance_Ranged(object sender, System.Collections.Generic.IEnumerable<IBeacon> e)
+		{
+			_beaconsList.ItemsSource = e; 
+		}
+
+		void Instance_RegionStatusChanged(object sender, BeaconRegionStatusChangedEventArgs e)
+		{
+			var debug = true;
+		}*/
+
+		void _scanButton_Clicked(object sender, EventArgs e)
+		{
+			/*EstimoteManager.Instance.StartRanging(new BeaconRegion("5739684d584f","f7826da6-4fa2-4e98-8024-bc5b71e0893e"));
+			EstimoteManager.Instance.StartRanging(new BeaconRegion("4f7234416c31","f7826da6-4fa2-4e98-8024-bc5b71e0893e"));
+			EstimoteManager.Instance.StartRanging(new BeaconRegion("c41821932394", "B9407F30-F5F8-466E-AFF9-25556B57FE6D", 25304, 1457));
+			EstimoteManager.Instance.StartRanging(new BeaconRegion("c9c43ff35719", "B9407F30-F5F8-466E-AFF9-25556B57FE6D", 25216, 1281));*/
+		}
+	}
+}
+
